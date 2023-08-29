94a9a6bea20057464c0379802fec25ebe1dba0fb
iamakashrout <iamakashrout@gmail.com>
2023-08-29 15:46:20 +0530

    assignment 1 and 2 updated

--- ASSIGNMENT 1/q2.cpp
+++ ASSIGNMENT 1/q2.cpp
@@ -1,66 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-map<string, vector<string>> mp;
-
 int main(){
-    while(true){
-        int q;
-        cout << "Input your operation: ('1' to create new contact, '2' to update existing contact, '3' for searching a contact, '4' for deleting a contact, '5' to close application, '6' for prefix searching)" << endl;
-        cin >> q;
-        if(q==1){ // creating contact
-            string name, phone;
-            cout << "Enter contact name: " << endl;
-            cin >> name;
-            cout << "Enter contact number: " << endl;
-            cin >> phone;
-            mp[name].push_back(phone);
-        }
-        else if(q==2){ // updating contact
-            string name, oldPhone, newPhone;
-            cout << "Enter contact name: " << endl;
-            cin >> name;
-            cout << "Enter old contact number: " << endl;
-            cin >> oldPhone;
-            cout << "Enter new contact number: " << endl;
-            cin >> newPhone;
-            for (int i = 0; i < mp[name].size(); i++){
-                if(mp[name][i]==oldPhone){
-                    mp[name][i] = newPhone;
-                }
-            }
-        }
-        else if(q==3){ // searching contact
-            string name;
-            cout << "Enter contact name: " << endl;
-            cin >> name;
-            cout << "Phone numbers of " << name << " are: " << endl;
-            for (int i = 0; i < mp[name].size(); i++){
-                cout << mp[name][i] << endl;
-            }
-        }
-        else if(q==4){ // deleting contact
-            string name;
-            cout << "Enter contact name: " << endl;
-            cin >> name;
-            mp.erase(name);
-        }
-        else if(q==5){ // closing application
-            break;
-        }
-        else if(q==6){ // prefix searching for names
-            string name;
-            cout << "Enter contact name: " << endl;
-            cin >> name;
-            int n = name.size();
-            for (auto it = mp.begin(); it!=mp.end(); it++){
-                string s = it->first;
-                int x = s.size();
-                if(x>=n && s.substr(0, n)==name){
-                    cout << s << endl;
-                }
-            }
-        }
+    int a, b, c;
+    cout << "Enter coefficient of x^2: " << endl;
+    cin >> a;
+    cout << "Enter coefficient of x: " << endl;
+    cin >> b;
+    cout << "Enter coefficient of constant: " << endl;
+    cin>>c;
+    float d = b*b-4*a*c;
+    if(d>0){
+        double root1=(-b+sqrt(d))/(2*a);
+        double root2=(-b-sqrt(d))/(2*a);
+        cout << root1 << " and " << root2 << endl;
+    }
+    else if(d<0){
+        double rootReal = (-b) / (2 * a);
+        double rootImg = (sqrt(abs(d))) / (2 * a);
+        cout << rootReal << " + " << rootImg << "i"
+             << " and " << rootReal << " - " << rootImg << "i" << endl;
+    }
+    else{
+        double root = (-b) / (2 * a);
+        cout << root << endl;
     }
     return 0;
 }
